621b42b4488e102b491098ca5cb6a698d7cc98d7
Lindo-123 <2092154@students.wits.ac.za>
2026-08-25 23:53:16 +0200

    Submit record function

--- contracts/Assignment_Smart_Contract.sol
+++ contracts/Assignment_Smart_Contract.sol
@@ -13,12 +13,12 @@ contract Assigment {
 
     // Submit emissions records (or hashes of emissions reports)
     struct EmissionRecord {
-        uint256 RecordID;
+        //uint256 RecordID;
         uint256 FacilityID;
-        uint64 Year; 
+        uint256 Year; 
         address submittedBy;
         uint256 Timestamp;
-        uint256 ReportHash;
+        bytes32 ReportHash;
         bool verified;
         address verifier;  // or auditor
     }
@@ -32,7 +32,7 @@ contract Assigment {
 
     //Events 
     event FacilityRegistered(uint256 FacilityID, string name, address owner); //triggered when a new facility is added
-    event RecordSubmitted(uint256 RecordID, uint256 FacilityID, uint256 ReportHash, address SubmittedBy); //triggered when emission is reported
+    event RecordSubmitted(uint256 RecordID, uint256 FacilityID, bytes32 ReportHash, address SubmittedBy); //triggered when emission is reported
     event RecordVerified(uint256 RecordID, address verifier);  //When auditor signs reported emissions
     event SubmitterChanged(address account, bool allowed); //submitter authorisation changes
     event VerifierChanged(address account, bool allowed); //verifier authority changes
@@ -80,6 +80,23 @@ contract Assigment {
         require(AuthSubmitter[msg.sender], "not authorised submitter");
         _;
     }
+
+    function submitRecord(
+        uint256 FacilityID, bytes32 ReportHash, uint256 Year) public onlySubmitter(){
+            require(FacilityID < facilities.length, "Facility does not exist");
+            require(ReportHash != bytes32(0), "Hash cannot be empty");
+            require(Year >= 2000 && Year <2100, "Invalid year");
+
+            records.push(EmissionRecord({
+                FacilityID: FacilityID, ReportHash: ReportHash, Year: Year, submittedBy: msg.sender, Timestamp: block.timestamp,
+                verified: false, verifier: address(0)
+
+            })); 
+            uint256 RecordID = records.length-1;
+            FacilityRecords[FacilityID].push(RecordID);
+
+            emit RecordSubmitted(RecordID, FacilityID, ReportHash, msg.sender);
+        }
 
     
     //Auditor verifying/approving record (more of a sign-off)
