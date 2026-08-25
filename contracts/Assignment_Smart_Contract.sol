ad00bc03ce97649e150a93a3d4ec94b0daca472b
Lindo-123 <2092154@students.wits.ac.za>
2026-08-25 20:12:07 +0200

    Facility and emission records struct

--- contracts/Assignment_Smart_Contract.sol
+++ contracts/Assignment_Smart_Contract.sol
@@ -1,3 +1,27 @@
 
-// testing commit
+
+contract Assigment {
+
+    // Register Manufacturing facilities
+    struct Facility {
+        string name;
+        address owner;
+
+    }
+
+    // Submit emissions records (or hashes of emissions reports)
+    struct EmissionRecord {
+        uint256 RecordID;
+        string FacilityName;
+        uint64 Date; 
+        address submittedBy;
+        uint256 ReportHash;
+
+    }
+
+
+
+
+}
+
 
