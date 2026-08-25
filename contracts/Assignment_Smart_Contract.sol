70cf7aacad10b7e120c89b3d33b45f48d3f0422c
Lindo-123 <2092154@students.wits.ac.za>
2026-08-25 21:29:30 +0200

    Mapping and Events creation

--- contracts/Assignment_Smart_Contract.sol
+++ contracts/Assignment_Smart_Contract.sol
@@ -12,13 +12,28 @@ contract Assigment {
     // Submit emissions records (or hashes of emissions reports)
     struct EmissionRecord {
         uint256 RecordID;
-        string FacilityName;
-        uint64 Date; 
+        uint256 FacilityID;
+        uint64 Year; 
         address submittedBy;
+        uint256 Timestamp;
         uint256 ReportHash;
-
+        bool verified;
+        address verifier;  // or auditor
     }
 
+    Facility[] public facilities; 
+    EmissionRecord[] public records;
+
+    mapping(uint256 => uint256[]) public FacilityRecords; //facility to record
+    mapping(address => bool) public AuthorisedSubmitter; 
+    mapping(address => bool) public AuthVerifier;   
+
+    //Events 
+    event FacilityRegistered(uint256 FacilityID, string name, address owner); //triggered when a new facility is added
+    event RecordSubmitted(uint256 RecordID, uint256 FacilityID, uint256 ReportHash, address SubmittedBy); //triggered when emission is reported
+    event RecordVerified(uint256 RecordID, address verifier);  //When auditor signs reported emissions
+    event SubmitterChanged(address account, bool allowed); //submitter authorisation changes
+    event VerifierChanged(address account, bool allowes); //verifier authority changes
 
 
 
